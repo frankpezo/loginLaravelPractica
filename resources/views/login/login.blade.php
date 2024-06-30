6cbcb98f419b43c792765f1abec8b3328d21a0a8
frankpezo <73926318+frankpezo@users.noreply.github.com>
2024-06-30 12:00:30 -0500

    first commit

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Login');
+
+@section('content')
+    <h1>Iniciar sesión</h1>
+
+    <button type="button">Iniciar sesión</button>
+    <button type="button">Registrarse</button>
+@endSection
