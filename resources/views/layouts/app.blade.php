62daa5895058fd36c4bce710689a3fa4d9b6e700
frankpezo <73926318+frankpezo@users.noreply.github.com>
2024-06-30 12:13:03 -0500

    Creación de archivos laravel y bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <title>@yield('title')</title>
 </head>
 
@@ -12,6 +13,10 @@
     @yield('content')
 
 
+
+    <!-- Scripts Bootstrap-->
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 
 </html>
